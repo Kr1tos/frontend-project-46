efd9840abae173fd394bb76339bf3265fdba54a7
Kr1tos <67792458+Kr1tos@users.noreply.github.com>
2023-03-02 21:39:28 +0300

    Update index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,19 +1,15 @@
 import plain from './plain.js';
 import stylish from './stylish.js';
 
-const formatters = {
-  plain,
-  json: JSON.stringify,
-  stylish: (data) => `{\n${stylish(data)}\n}`,
-};
-
 const formatter = (data, style = 'stylish') => {
-  const format = formatters[style];
-  if (!format) {
-    throw new Error(`Invalid style: ${style}`);
+  switch (style) {
+    case 'plain':
+      return plain(data);
+    case 'json':
+      return JSON.stringify(data);
+    default:
+      return `{\n${stylish(data)}\n}`;
   }
-
-  return format(data);
 };
 
 export default formatter;
